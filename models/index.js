92e6a6d363694079e9f3dc60a0421c0417727bc5
SamanthaJoy13 <tavaresjsamantha52@icloud.com>
2023-07-24 12:50:58 -0700

    finished models/index

--- models/index.js
+++ models/index.js
@@ -1,20 +1,48 @@
-// import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
-// Categories have many Products
+Product.belongsTo(Category, {
 
-// Products belongToMany Tags (through ProductTag)
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
 
-// Tags belongToMany Products (through ProductTag)
+});
+
+
+Category.hasMany(Product, {
+
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+
+});
+
+
+Product.belongsToMany(Tag, {
+
+  through: ProductTag,
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+
+});
+
+
+Tag.belongsToMany(Product, {
+
+  through: ProductTag,
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE',
+
+});
+
 
 module.exports = {
+
   Product,
   Category,
   Tag,
   ProductTag,
+
 };
